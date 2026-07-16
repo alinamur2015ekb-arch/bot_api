41273e8b977faccbb6fd5a769b566f3695957fb7
alinamur2015ekb-arch <alinamur2015ekb@gmail.com>
2026-07-16 13:53:52 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,21 +4,30 @@ from aiogram import Bot, Dispatcher
 import asyncio
 from hendlers import router as hendlers_router
 
+async def pinger():
+    """Функция, которая сама пингует сайт бота, чтобы он не спал"""
+    await asyncio.sleep(10)
+    async with aiohttp.ClientSession() as session:
+        while True:
+            try:
+                async with session.get('https://telegramm-bot-rpin.onrender.com') as response:
+                    print(f"Пинг выполнен! Статус: {response.status}")
+            except Exception as e:
+                print(f"Ошибка пинга: {e}")
+            await asyncio.sleep(600)
 
 load_dotenv()
 TOKEN = os.getenv("token")
-
 
 dp = Dispatcher()
 dp.include_routers(
     hendlers_router #роутер
 )
 
-
 async def main():
     bot = Bot(TOKEN)
-     #инициализация бд
     await dp.start_polling(bot) 
+    asyncio.create_task(pinger())
     
 if __name__ == "__main__":
     try:
